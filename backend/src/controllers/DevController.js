b878a27cebd885d2333b8bd40e645674b6dadc9c
Lucas <lucasbezerra1701@gmail.com>
2020-02-17 10:48:46 -0300

    Backend Application

---
+++ backend/src/controllers/DevController.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-undef */
+/* eslint-disable camelcase */
+const axios = require('axios');
+const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
+
+module.exports = {
+  async index(request, response) {
+    const devs = await Dev.find();
+
+    return response.json(devs);
+  },
+
+  async store(request, response) {
+    const { github_username, techs, latidude, longitude } = request.body;
+
+    let dev = await Dev.findOne({ github_username });
+
+    if (!dev) {
+      const apiResponse = await axios.get(
+        `https://api.github.com/users/${github_username}`
+      );
+
+      const { name = login, avatar_url, bio } = apiResponse.data;
+
+      const techsArray = parseStringAsArray(techs);
+
+      const location = {
+        type: 'Point',
+        coordinates: [longitude, latidude]
+      };
+
+      dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+        location
+      });
+    }
+
+    return response.json(dev);
+  }
+};
